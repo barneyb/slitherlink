00adb387cf9d61d7f866fbb6b4c3fed72e854abb
Barney Boisvert <bboisvert@gmail.com>
2014-12-06 12:38:41 -0800

    complete (though minimally tested) square grid factory

--- src/main/java/com/barneyb/slitherlink/grid/SquareGridFactory.groovy
+++ src/main/java/com/barneyb/slitherlink/grid/SquareGridFactory.groovy
@@ -12,9 +12,72 @@ class SquareGridFactory implements GridFactory {
         if (height <= 0) {
             throw new IllegalArgumentException("Invalid height: $height")
         }
-        def g = new Grid()
-        // todo: finish me!
+        def g = new Grid(width, height)
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                def f = new Face(4)
+                f.dots << getDot(g, x, y)
+                f.dots << getDot(g, x + 1, y)
+                f.dots << getDot(g, x + 1, y + 1)
+                f.dots << getDot(g, x, y + 1)
+                g.faces << f
+            }
+        }
+        makeConsistent(g)
+    }
+
+    protected Grid makeConsistent(Grid g) {
+        def es = new TreeSet<Edge>()
+        def getEdge = { Dot a, Dot b ->
+            def e = a < b ? new Edge(a, b) : new Edge(b, a)
+            def existing = es.find { it == e }
+            if (! existing) {
+                es.add(e)
+                existing = e
+            }
+            existing
+        }
+
+        // Face.edges
+        g.faces.each { f ->
+            f.dots.eachWithIndex { d, i ->
+                def edge = getEdge(d, f.dots[(i + 1) % f.order])
+                if (edge.start == d) {
+                    assert ! edge.right
+                    edge.right = f
+                } else {
+                    assert ! edge.left
+                    edge.left = f
+                }
+                f.edges << edge
+                d.faces << f
+            }
+        }
+        g.edges.addAll(es)
+
+        // Dot.edges
+        g.dots.each { d ->
+            def list = d.faces.collect { f ->
+                f.edges.findAll {
+                    it.start == d || it.end == d
+                }
+            }.flatten().unique()
+            while (list.min() != list.first()) {
+                list.add list.remove(0)
+            }
+            d.edges.addAll list
+        }
+
         g
+    }
+
+    protected Dot getDot(Grid g, int x, int y) {
+        def i = y * (g.width + 1) + x
+        def d = g.dots[i]
+        if (! d) {
+            d = g.dots[i] = new Dot(x, y)
+        }
+        d
     }
 
 }
--- src/test/java/com/barneyb/slitherlink/grid/SquareGridFactoryTest.groovy
+++ src/test/java/com/barneyb/slitherlink/grid/SquareGridFactoryTest.groovy
@@ -21,60 +21,72 @@ class SquareGridFactoryTest {
 
     @Test
     void oneByOne() {
+        /*
+         0  0  1
+          +---+
+        1 | 0 | 2
+          +---+
+         2  3  3
+         */
         def g = new SquareGridFactory().create(1, 1)
         // counts
-        assert g.dots.size() == 4
-        assert g.edges.size() == 4
-        assert g.faces.size() == 1
+        assert g.dots.size() == 4 // (x+1) * (y+1)
+        assert g.edges.size() == 4 // (x+1) * y + x * (y+1)
+        assert g.faces.size() == 1 // x * y
 
         def face = g.faces.first()
-        def (e1, e2, e3, e4) = g.edges
-        def (d1, d2, d3, d4) = g.dots
+        def (Edge e0, e1, e2, e3) = g.edges
+        def (Dot d0, d1, d2, d3) = g.dots
+
+        assert d0 == new Dot(0, 0)
+        assert d1 == new Dot(1, 0)
+        assert d2 == new Dot(0, 1)
+        assert d3 == new Dot(1, 1)
+
+        assert e0 == new Edge(d0, d1)
+        assert e1 == new Edge(d0, d2)
+        assert e2 == new Edge(d1, d3)
+        assert e3 == new Edge(d2, d3)
 
         // the face
-        assert face.edges == g.edges
-        assert face.dots == g.dots
+        assert face.order == 4
+        assert face.edges as Set == g.edges as Set
+        assert face.edges == [e0, e2, e3, e1]
+        assert face.dots as Set == g.dots as Set
+        assert face.dots == [d0, d1, d3, d2]
         assert face.clue == null
 
-        // d1
-        assert d1.edges == [e4, e1]
-        assert d1.faces == [face]
+        assert e0.start == d0
+        assert e0.end == d1
+        assert e0.left == null
+        assert e0.right == face
 
-        // d2
-        assert d2.edges == [e1, e2]
-        assert d2.faces == [face]
-
-        // d3
-        assert d3.edges == [e2, e3]
-        assert d3.faces == [face]
-
-        // d4
-        assert d4.edges == [e3, e4]
-        assert d4.faces == [face]
-
-        // e1
-        assert e1.start == d1
+        assert e1.start == d0
         assert e1.end == d2
-        assert e1.left == null
-        assert e1.right == face
+        assert e1.left == face
+        assert e1.right == null
 
-        // e2
-        assert e2.start == d2
+        assert e2.start == d1
         assert e2.end == d3
-        assert e2.left == face
-        assert e2.right == null
+        assert e2.left == null
+        assert e2.right == face
 
-        // e3
-        assert e3.start == d3
-        assert e3.end == d4
+        assert e3.start == d2
+        assert e3.end == d3
         assert e3.left == face
         assert e3.right == null
 
-        // e4
-        assert e4.start == d4
-        assert e4.end == d1
-        assert e4.left == null
-        assert e4.right == face
+        assert d0.faces == [face]
+        assert d0.edges == [e0, e1]
+
+        assert d1.faces == [face]
+        assert d1.edges == [e0, e2]
+
+        assert d2.faces == [face]
+        assert d2.edges == [e1, e3]
+
+        assert d3.faces == [face]
+        assert d3.edges == [e2, e3]
     }
 
 }
