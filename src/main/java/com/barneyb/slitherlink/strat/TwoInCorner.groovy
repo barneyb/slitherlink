4c516aca16da161f6d9dbb8dda2e2a11900b04ca
Barney Boisvert <bboisvert@gmail.com>
2018-02-18 14:54:23 -0800

    cleaning up

--- src/main/java/com/barneyb/slitherlink/strat/TwoInCorner.groovy
+++ src/main/java/com/barneyb/slitherlink/strat/TwoInCorner.groovy
@@ -1,7 +1,6 @@
 package com.barneyb.slitherlink.strat
 
 import com.barneyb.slitherlink.CellCoord
-import com.barneyb.slitherlink.Dir
 import com.barneyb.slitherlink.EdgeCoord
 import com.barneyb.slitherlink.EdgeState
 import com.barneyb.slitherlink.Move
@@ -9,6 +8,7 @@ import com.barneyb.slitherlink.MoveImpl
 import com.barneyb.slitherlink.Puzzle
 import com.barneyb.slitherlink.StaticStrategy
 
+import static com.barneyb.slitherlink.Dir.*
 /**
  *
  *
@@ -18,26 +18,25 @@ class TwoInCorner implements StaticStrategy {
 
     Move nextMove(Puzzle p) {
         def corners = [
-            (p.cellCoord(0, 0))                  : [
-                p.edgeCoord(0, 1, Dir.NORTH),
-                p.edgeCoord(1, 0, Dir.WEST)
+            (p.cellCoord(0, 0)): [
+                p.edgeCoord(0, 1, NORTH),
+                p.edgeCoord(1, 0, WEST)
             ],
-            (p.cellCoord(0, p.cols - 1))         : [
-                p.edgeCoord(0, p.cols - 2, Dir.NORTH),
-                p.edgeCoord(1, p.cols - 1, Dir.EAST)
+            (p.cellCoord(0, p.cols - 1)): [
+                p.edgeCoord(0, p.cols - 2, NORTH),
+                p.edgeCoord(1, p.cols - 1, EAST)
             ],
             (p.cellCoord(p.rows - 1, p.cols - 1)): [
-                p.edgeCoord(p.rows - 1, p.cols - 2, Dir.SOUTH),
-                p.edgeCoord(p.rows - 2, p.cols - 1, Dir.EAST)
+                p.edgeCoord(p.rows - 1, p.cols - 2, SOUTH),
+                p.edgeCoord(p.rows - 2, p.cols - 1, EAST)
             ],
-            (p.cellCoord(p.rows - 1, 0))         : [
-                p.edgeCoord(p.rows - 1, 1, Dir.SOUTH),
-                p.edgeCoord(p.rows - 2, 0, Dir.WEST)
+            (p.cellCoord(p.rows - 1, 0)): [
+                p.edgeCoord(p.rows - 1, 1, SOUTH),
+                p.edgeCoord(p.rows - 2, 0, WEST)
             ]
         ]
         for (CellCoord cc : corners.keySet()) {
-            def c = cc.clue()
-            if (c == 2) {
+            if (cc.clue() == 2) {
                 for (EdgeCoord ec : corners[cc]) {
                     if (ec.state() != EdgeState.ON) {
                         return new MoveImpl(this, ec, EdgeState.ON)
