9b03d1a9a9bb44637bdd92f1d864e26773a6f125
barneyb <bboisvert@gmail.com>
2018-02-17 20:35:42 -0800

    renamed ContextFree to Idempotent

--- src/main/java/com/barneyb/slitherlink/Idempotent.groovy
+++ src/main/java/com/barneyb/slitherlink/Idempotent.groovy
@@ -3,10 +3,11 @@ package com.barneyb.slitherlink
 /**
  * I am a marker interface for Strategy implementations which do not rely on
  * the state of the board, they only rely on the puzzle itself (dimensions and
- * clues, but not edge state).
+ * clues, but not edge state). Thus they may be run a single time without
+ * limitting their capabilities.
  *
  * @author barneyb
  */
-interface ContextFree {
+interface Idempotent {
 
 }
