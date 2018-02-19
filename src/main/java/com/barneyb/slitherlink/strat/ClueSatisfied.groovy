8f36b9030e557e69f556c84aa6d03952afe3ea02
Barney Boisvert <bboisvert@gmail.com>
2018-02-19 15:31:59 -0800

    ClueSatisfied is now multi-move

--- src/main/java/com/barneyb/slitherlink/strat/ClueSatisfied.groovy
+++ src/main/java/com/barneyb/slitherlink/strat/ClueSatisfied.groovy
@@ -1,30 +1,24 @@
 package com.barneyb.slitherlink.strat
 
-import com.barneyb.slitherlink.EdgeCoord
 import com.barneyb.slitherlink.EdgeState
 import com.barneyb.slitherlink.Move
-import com.barneyb.slitherlink.MoveImpl
+import com.barneyb.slitherlink.MultiMoveStrategy
 import com.barneyb.slitherlink.Puzzle
-import com.barneyb.slitherlink.SingleMoveStrategy
-
 /**
  *
  * @author bboisvert
  */
-class ClueSatisfied implements SingleMoveStrategy {
+class ClueSatisfied implements MultiMoveStrategy {
 
-    Move nextMove(Puzzle p) {
+    @Override
+    List<Move> nextMoves(Puzzle p) {
         for (cc in p.clueCells()) {
             def edges = cc.edges()
             if (edges.count { it.state == EdgeState.ON } == cc.clue) {
-                for (EdgeCoord ec : edges) {
-                    if (ec.state == EdgeState.UNKNOWN) {
-                        return new MoveImpl(this, ec, EdgeState.OFF)
-                    }
-                }
+                def mc = Utils.edgesOffIf(edges, EdgeState.UNKNOWN)
+                if (mc) return mc
             }
         }
         null
     }
-
 }
