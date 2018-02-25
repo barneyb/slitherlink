364b29756eb12fefbbcb6a642f6c64c21a9887ff
Barney Boisvert <bboisvert@gmail.com>
2018-02-25 11:15:00 -0800

    support loading solutions for krazydad puzzles, as well as the clues

--- kotlin/src/main/kotlin/com/barneyb/slitherlink/io/krazydad.kt
+++ kotlin/src/main/kotlin/com/barneyb/slitherlink/io/krazydad.kt
@@ -1,24 +1,101 @@
 package com.barneyb.slitherlink.io
 
+import com.barneyb.slitherlink.BLANK
+import com.barneyb.slitherlink.EdgeState
+import com.barneyb.slitherlink.OFF
+import com.barneyb.slitherlink.ON
 import com.barneyb.slitherlink.Puzzle
 
 private const val DOT = '.'
 private const val ZERO = '0'
 
-fun krazydad(spec: String): Puzzle {
-    val size = Math.sqrt(spec.length.toDouble()).toInt()
-    if (size * size != spec.length) {
+fun krazydad(puzz: String): Puzzle {
+    val size = Math.sqrt(puzz.length.toDouble()).toInt()
+    if (size * size != puzz.length) {
         throw IllegalArgumentException("Only square grids may be constructed w/out explicit dimensions")
     }
-    return krazydad(size, size, spec)
+    return krazydad(size, size, puzz)
 }
 
-fun krazydad(rows: Int, cols: Int, spec: String): Puzzle {
-    if (spec.length != rows * cols) {
+fun krazydad(puzz: String, solved: String): Puzzle {
+    val p = krazydad(puzz)
+    solve(p, solved)
+    return p
+}
+
+private fun solve(p: Puzzle, solved: String) {
+    val edges = solved
+        .map {
+            it.toString().toInt(16)
+        }.map { nyb ->
+            (0..3).map { b ->
+                (nyb and (1 shl b)) != 0
+            }
+        }
+        .flatten()
+    val itr = edges
+        .map { if (it) ON else OFF }
+        .iterator()
+    for (r in 0 until p.humanRows) {
+        for (c in 0 until p.humanCols) {
+            if (r == 0) {
+                p.state(0, c * 2 + 1, itr.next())
+            }
+            p.state(r * 2 + 1, c * 2 + 2, itr.next())
+            p.state(r * 2 + 2, c * 2 + 1, itr.next())
+            if (c == 0) {
+                p.state(r * 2 + 1, 0, itr.next())
+            }
+        }
+    }
+}
+
+fun krazydad(p: Puzzle): Pair<String, String> {
+    val puzz = StringBuilder()
+    for (r in 1 until p.gridRows step 2) {
+        for (c in 1 until p.gridCols step 2) {
+            val clue = p.clue(r, c)
+            puzz.append(if (clue == BLANK) DOT else clue)
+        }
+    }
+    val edges = mutableListOf<Boolean>()
+    fun doEdge(s: EdgeState) = when (s) {
+        OFF  -> edges.add(false)
+        ON   -> edges.add(true)
+        else -> throw IllegalArgumentException("KrazyDad can't represent puzzles w/ unknown edges")
+    }
+    for (r in 0 until p.humanRows) {
+        for (c in 0 until p.humanCols) {
+            if (r == 0) {
+                doEdge(p.state(0, c * 2 + 1))
+            }
+            doEdge(p.state(r * 2 + 1, c * 2 + 2))
+            doEdge(p.state(r * 2 + 2, c * 2 + 1))
+            if (c == 0) {
+                doEdge(p.state(r * 2 + 1, 0))
+            }
+        }
+    }
+    val solved = StringBuilder()
+    var temp = 0
+    edges.forEachIndexed { i, on ->
+        if (on) {
+            temp += (1 shl (i % 4))
+        }
+        if (i % 4 == 3) {
+            solved.append(temp.toString(16))
+            temp = 0
+        }
+    }
+    return Pair(puzz.toString(), solved.toString().toUpperCase())
+}
+
+fun krazydad(rows: Int, cols: Int, puzz: String): Puzzle {
+    if (puzz.length != rows * cols) {
         throw IllegalArgumentException("Spec doesn't match dimensions")
     }
     val p = Puzzle(rows, cols)
-    val cs = spec.toCharArray()
+    val cs = puzz.toCharArray()
     for (r in 0 until rows) {
         for (c in 0 until cols) {
             val clue = cs[r * cols + c]
@@ -27,5 +104,11 @@ fun krazydad(rows: Int, cols: Int, spec: String): Puzzle {
             }
         }
     }
+    return p
+}
+
+fun krazydad(rows: Int, cols: Int, puzz: String, solved: String): Puzzle {
+    val p = krazydad(rows, cols, puzz)
+    solve(p, solved)
     return p
 }
--- kotlin/src/test/kotlin/com/barneyb/slitherlink/io/KrazydadKtTest.kt
+++ kotlin/src/test/kotlin/com/barneyb/slitherlink/io/KrazydadKtTest.kt
@@ -1,63 +1,79 @@
 package com.barneyb.slitherlink.io
 
 import com.barneyb.slitherlink.BLANK
-import com.barneyb.slitherlink.ONE
-import com.barneyb.slitherlink.THREE
-import com.barneyb.slitherlink.TWO
-import com.barneyb.slitherlink.ZERO
 import org.junit.Test
 import kotlin.test.assertEquals
 
+private const val KD_7x7_d1_V1_B1_P1_PUZZ =
+    "222331...02.3........33..33...2.2..3.2.22.3.2..3."
+private const val KD_7x7_d1_V1_B1_P1_SOLVED = "69970A5CE4516DBE233ED26CC7ED"
+
 class KrazydadKtTest {
+
+    @Test
+    fun withEdges() {
+        // KD_Slitherlink_7x7_d1_V1-B1-P1
+        val p = krazydad(KD_7x7_d1_V1_B1_P1_PUZZ, KD_7x7_d1_V1_B1_P1_SOLVED)
+        println(p)
+        assert_KD_7x7_d1_V1_B1_P1(p)
+    }
+
+    @Test
+    fun serialize() {
+        val p = krazydad(KD_7x7_d1_V1_B1_P1_PUZZ, KD_7x7_d1_V1_B1_P1_SOLVED)
+        val (puzz, solved) = krazydad(p)
+        assertEquals(KD_7x7_d1_V1_B1_P1_PUZZ, puzz)
+        assertEquals(KD_7x7_d1_V1_B1_P1_SOLVED, solved)
+    }
 
     @Test
     fun cornerOfSomething() {
         val p = krazydad("2223..02....33..")
         println(p)
-        assertEquals(TWO, p.humanCell(0, 0).clue)
-        assertEquals(TWO, p.humanCell(0, 1).clue)
-        assertEquals(TWO, p.humanCell(0, 2).clue)
-        assertEquals(THREE, p.humanCell(0, 3).clue)
-        assertEquals(BLANK, p.humanCell(1, 0).clue)
-        assertEquals(BLANK, p.humanCell(1, 1).clue)
-        assertEquals(ZERO, p.humanCell(1, 2).clue)
-        assertEquals(TWO, p.humanCell(1, 3).clue)
-        assertEquals(BLANK, p.humanCell(2, 0).clue)
-        assertEquals(BLANK, p.humanCell(2, 1).clue)
-        assertEquals(BLANK, p.humanCell(2, 2).clue)
-        assertEquals(BLANK, p.humanCell(2, 3).clue)
-        assertEquals(THREE, p.humanCell(3, 0).clue)
-        assertEquals(THREE, p.humanCell(3, 1).clue)
-        assertEquals(BLANK, p.humanCell(3, 2).clue)
-        assertEquals(BLANK, p.humanCell(3, 3).clue)
+        assertEquals(2, p.humanClue(0, 0))
+        assertEquals(2, p.humanClue(0, 1))
+        assertEquals(2, p.humanClue(0, 2))
+        assertEquals(3, p.humanClue(0, 3))
+        assertEquals(BLANK, p.humanClue(1, 0))
+        assertEquals(BLANK, p.humanClue(1, 1))
+        assertEquals(0, p.humanClue(1, 2))
+        assertEquals(2, p.humanClue(1, 3))
+        assertEquals(BLANK, p.humanClue(2, 0))
+        assertEquals(BLANK, p.humanClue(2, 1))
+        assertEquals(BLANK, p.humanClue(2, 2))
+        assertEquals(BLANK, p.humanClue(2, 3))
+        assertEquals(3, p.humanClue(3, 0))
+        assertEquals(3, p.humanClue(3, 1))
+        assertEquals(BLANK, p.humanClue(3, 2))
+        assertEquals(BLANK, p.humanClue(3, 3))
     }
 
     @Test
     fun twoByFour() {
         val p = krazydad(2, 4, ".11.....")
         println(p)
-        assertEquals(BLANK, p.humanCell(0, 0).clue)
-        assertEquals(ONE, p.humanCell(0, 1).clue)
-        assertEquals(ONE, p.humanCell(0, 2).clue)
-        assertEquals(BLANK, p.humanCell(0, 3).clue)
-        assertEquals(BLANK, p.humanCell(1, 0).clue)
-        assertEquals(BLANK, p.humanCell(1, 1).clue)
-        assertEquals(BLANK, p.humanCell(1, 2).clue)
-        assertEquals(BLANK, p.humanCell(1, 3).clue)
+        assertEquals(BLANK, p.humanClue(0, 0))
+        assertEquals(1, p.humanClue(0, 1))
+        assertEquals(1, p.humanClue(0, 2))
+        assertEquals(BLANK, p.humanClue(0, 3))
+        assertEquals(BLANK, p.humanClue(1, 0))
+        assertEquals(BLANK, p.humanClue(1, 1))
+        assertEquals(BLANK, p.humanClue(1, 2))
+        assertEquals(BLANK, p.humanClue(1, 3))
     }
 
     @Test
     fun fourByTwo() {
         val p = krazydad(4, 2, ".11.....")
         println(p)
-        assertEquals(BLANK, p.humanCell(0, 0).clue)
-        assertEquals(ONE, p.humanCell(0, 1).clue)
-        assertEquals(ONE, p.humanCell(1, 0).clue)
-        assertEquals(BLANK, p.humanCell(1, 1).clue)
-        assertEquals(BLANK, p.humanCell(2, 0).clue)
-        assertEquals(BLANK, p.humanCell(2, 1).clue)
-        assertEquals(BLANK, p.humanCell(3, 0).clue)
-        assertEquals(BLANK, p.humanCell(3, 1).clue)
+        assertEquals(BLANK, p.humanClue(0, 0))
+        assertEquals(1, p.humanClue(0, 1))
+        assertEquals(1, p.humanClue(1, 0))
+        assertEquals(BLANK, p.humanClue(1, 1))
+        assertEquals(BLANK, p.humanClue(2, 0))
+        assertEquals(BLANK, p.humanClue(2, 1))
+        assertEquals(BLANK, p.humanClue(3, 0))
+        assertEquals(BLANK, p.humanClue(3, 1))
     }
 
 }
