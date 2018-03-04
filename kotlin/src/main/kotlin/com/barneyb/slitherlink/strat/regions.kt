6727a422d934fe70de96b14a964e61c7c654b03b
barneyb <bboisvert@gmail.com>
2018-03-04 11:33:57 -0800

    factor out `getRegions(Puzzle)` and remove debugging

--- kotlin/src/main/kotlin/com/barneyb/slitherlink/strat/regions.kt
+++ kotlin/src/main/kotlin/com/barneyb/slitherlink/strat/regions.kt
@@ -6,8 +6,6 @@ import com.barneyb.slitherlink.OFF
 import com.barneyb.slitherlink.ON
 import com.barneyb.slitherlink.Puzzle
 import com.barneyb.slitherlink.UNKNOWN
-import com.barneyb.slitherlink.Visualizer
-import com.barneyb.slitherlink.humanState
 import java.util.*
 import kotlin.coroutines.experimental.buildSequence
 
@@ -15,47 +13,14 @@ import kotlin.coroutines.experimental.buildSequence
  * If exactly one end on the border of a region has a [UNKNOWN] edge both into
  * and out of the region, the parity of the total number of ends on the
  * region's border indicates whether the target end must turn in or out.
- *
- * A region is defind as "all the cells reachable without crossing a known (ON
- * or OFF) edge."
  */
 fun onlyInOrOutEndOnRegionBoundary(p: Puzzle) = buildSequence<Move> {
-    val regions = mutableListOf<Set<Cell>>()
-    val visited = mutableSetOf<Cell>()
-    for (start in p.cells()) {
-        if (!visited.add(start)) {
-            // already in there
-            continue
-        }
-        val region = mutableSetOf(start)
-        val queue: Queue<Cell> = LinkedList(region)
-        queue.add(start)
-        while (queue.isNotEmpty()) {
-            val c = queue.poll()!!
-            visited.add(c)
-            for (e in c.edges(UNKNOWN)) {
-                if (e.hasOpposedCell(c)) {
-                    val other = e.opposedCell(c)
-                    if (region.add(other)) {
-                        queue.add(other)
-                    }
-                }
-            }
-        }
-        if (region.size > 1) {
-            regions.add(region)
-        }
-    }
+    val regions = getRegions(p)
     if (regions.size == 1) {
         // nothing to do
         return@buildSequence
     }
-    println("${regions.size} region(s)")
-    fun String.pcol(c: Collection<*>) =
-        println(" $this : ${c.size} : $c")
     for (r in regions.sortedBy { it.size }) {
-        val v = Visualizer(p)
-        v.highlight("region", r)
         val border = r
             .map {
                 it.edges
@@ -66,7 +31,6 @@ fun onlyInOrOutEndOnRegionBoundary(p: Puzzle) = buildSequence<Move> {
             }
             .flatten()
             .toSet()
-        v.highlight("border", border)
         val edgeDots = border
             .map {
                 listOf(
@@ -98,14 +62,11 @@ fun onlyInOrOutEndOnRegionBoundary(p: Puzzle) = buildSequence<Move> {
             }
             .map { it.second }
             .toSet()
-        "edge:dots".pcol(edgeDots)
-        v.highlight("flip-flops", flipFlops)
         if (flipFlops.size != 1) {
             // multiple flip-flop points, so can't make a determination w/ counts alone
             continue
         }
         val regionEdges = r.map { it.edges }.flatten().toSet()
-        v.highlight("edges", regionEdges)
         val regionEndDots = edgeDots
             .map { it.second }
             .toSet()
@@ -118,8 +79,6 @@ fun onlyInOrOutEndOnRegionBoundary(p: Puzzle) = buildSequence<Move> {
                 // we already know there's exactly one that has a choice
                 regionEdges.intersect(it.edges(UNKNOWN)).isNotEmpty()
             }
-        v.highlight("region ends", regionEndDots)
-        v.show()
 
 
         // there is a potential egress over against the edge in #medium20x20v1b1p1
@@ -131,9 +90,41 @@ fun onlyInOrOutEndOnRegionBoundary(p: Puzzle) = buildSequence<Move> {
         val dot = flipFlops.first()
         val target = dot.edges(UNKNOWN).intersect(regionEdges)
         val state = if (regionEndDots.size % 2 == 0) ON else OFF
-        println("set $target to ${humanState(state)}")
-        println(p)
         setTo(target, state)
         break
     }
+}
+
+/**
+ * I return all multi-cell regions on the board. A region is defined as "all
+ * the cells reachable without crossing a known ([ON] or [OFF]) edge."
+ */
+private fun getRegions(p: Puzzle): List<Set<Cell>> {
+    val regions = mutableListOf<Set<Cell>>()
+    val visited = mutableSetOf<Cell>()
+    for (start in p.cells()) {
+        if (!visited.add(start)) {
+            // already in there
+            continue
+        }
+        val region = mutableSetOf(start)
+        val queue: Queue<Cell> = LinkedList(region)
+        queue.add(start)
+        while (queue.isNotEmpty()) {
+            val c = queue.poll()!!
+            visited.add(c)
+            for (e in c.edges(UNKNOWN)) {
+                if (e.hasOpposedCell(c)) {
+                    val other = e.opposedCell(c)
+                    if (region.add(other)) {
+                        queue.add(other)
+                    }
+                }
+            }
+        }
+        if (region.size > 1) {
+            regions.add(region)
+        }
+    }
+    return regions
 }
